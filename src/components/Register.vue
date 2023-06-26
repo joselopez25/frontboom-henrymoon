91234632fe2b1e477d484a287632408a36e42cd2
joselopez25 <jose_lopez25@outlook.com>
2023-06-26 01:34:43 -0500

    stiles

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -37,9 +37,37 @@ onMounted(()=>{
 </script>
 
 <template>
-  <label for="name">Nombre:</label>
-  <input id="name" placeholder="Ingresa tu nombre" v-model="usuario"/>
-  <button @click="handleSubmit" >Ingresar</button>
+  <label for="name">Nombre: </label>
+  <input class="input" id="name" placeholder="Ingresa tu nombre" v-model="usuario"/>
+  <button class="ingreso" @click="handleSubmit" >Ingresar</button>
   <p v-if="error" >{{ error }}</p>
 </template>
+
+<style scoped>
+.input{
+  width: 300px;
+  height: 30px;
+  margin-right: 10px;
+  border: 2px solid #fefefe;
+  border-radius: 5px;
+  font-size: 16px;
+  margin-left: 10px;
+}
+
+.ingreso{
+  background-color: #2d2d2d;
+  border-radius: 8px;
+  border: 2px solid transparent;
+  padding: 0.6em 1.2em;
+  font-size: 1em;
+  font-weight: 500;
+  font-family: inherit;
+  cursor: pointer;
+  transition: border-color 0.25s;
+}
+
+.ingreso:hover {
+  border-color: #ffffff;
+}
+</style>
 
